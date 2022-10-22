3a010c133bad7a184ace4677139555d7673950bb
Zhenya <y.s.skiba@student.khai.edu>
2022-10-22 21:20:19 +0300

    ChatBot 4

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -17,5 +17,10 @@ public class ChatBot {
         int remainder7 = scan.nextInt();
         int age = (remainder3*70+remainder5*21+remainder7*15)%105;
         System.out.println("Your age is "+ age +"; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want");
+        int number = scan.nextInt();
+        for(int i =0 ; i <=number; i++){
+            System.out.println(i+"!");
+        }
     }
         }
