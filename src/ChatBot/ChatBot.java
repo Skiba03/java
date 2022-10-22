f5e9f1f41af102e2d7994ac43bb7409c973383d4
Zhenya <y.s.skiba@student.khai.edu>
2022-10-22 19:17:11 +0300

    ChatBot 2

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,9 +1,17 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
-        System.out.println("Hello!My name is Bot");
-        System.out.println("I was created in 2022");
+        Scanner stringScanner = new Scanner(System.in);
+        System.out.println("Hello!My name is Bot.");
+        System.out.println("I was created in 2022.");
+        System.out.println("Please,remind me your name.");
+        String input = stringScanner.next();
+        System.out.println("What a great name you have,"+ input);
+        stringScanner.close();
+
     }
 
         }
