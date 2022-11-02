30f0c26c45a8806d9453e3d172d5bddb5356395e
Zhenya <y.s.skiba@student.khai.edu>
2022-11-02 15:53:13 +0200

    Hangman 2

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,8 +1,19 @@
 package Hangman;
 
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon.");
+        System.out.println("HANGMAN");
+        System.out.println("Guess the word:");
+        String word = scan.next();
+        if (word.equals("java")) {
+            System.out.println("You survived");
+        } else{
+            System.out.println("You lost");
+        }
     }
 }
