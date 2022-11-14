32f9d2100ee7dc0cbe730efb6491bf7b6ad178b4
Zhenya <y.s.skiba@student.khai.edu>
2022-11-14 21:17:07 +0200

    Hangman 5

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,28 +1,77 @@
 package Hangman;
 
 import java.util.Scanner;
-import java.util.Random;
 
 public class Hangman {
+
+    private static String[] words = {"java","python","javascript","kotlin",};
+    private static String word = words[(int) (Math.random() * words.length)];
+    private static String minus = new String(new char[word.length()]).replace("\0", "-");
+    private static int count = 0;
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String[] words = new String[]{"java","python","javascript","kotlin",};
-        int random = new Random().nextInt(words.length);
         System.out.println("HANGMAN");
-        String str1 = (words[random]);
-        char letter1 = str1.charAt(0);
-        String str2 = (words[random]);
-        char letter2 = str2.charAt(1);
-        int amount = 0;
-        for(int i = 2; i < (words[random]).length(); i++) {
-            amount++;
+
+        while (count < 8 && minus.contains("-")) {
+            System.out.println(minus);
+            String guess = scan.next();
+            hang(guess);
         }
-        System.out.println("Guess the word "+ letter1 + letter2 + amount);
-        String word = scan.next();
-        if (word.equals(words[random])) {
-            System.out.println("You survived");
-        } else{
-            System.out.println("You lost");
+        scan.close();
+    }
+
+    public static void hang(String guess) {
+        String newminus = "";
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == guess.charAt(0)) {
+                newminus += guess.charAt(0);
+            } else if (minus.charAt(i) != '-') {
+                newminus += word.charAt(i);
+            } else {
+                newminus += "-";
+            }
+        }
+
+        if (minus.equals(newminus)) {
+            count++;
+            Try();
+        }else {
+            minus = newminus;
+            count++;
+        }
+        if (minus.equals(word)) {
+            System.out.println("You win!");
+            System.out.println("Thanks for playing!");
+            System.out.println("We'll see how well you did in the next stage");
+        }
+    }
+
+    public static void Try() {
+        if (count == 1) {
+            System.out.println("That letter doesn't appear in the word");
+        }
+        if (count == 2) {
+            System.out.println("That letter doesn't appear in the word");
+        }
+        if (count == 3) {
+            System.out.println("That letter doesn't appear in the word");
+        }
+        if (count == 4) {
+            System.out.println("That letter doesn't appear in the word");
+        }
+        if (count == 5) {
+            System.out.println("That letter doesn't appear in the word");
+        }
+        if (count == 6) {
+            System.out.println("That letter doesn't appear in the word");
+        }
+        if (count == 7) {
+            System.out.println("That letter doesn't appear in the word");
+        }
+        if (count == 8) {
+            System.out.println("Thanks for playing");
+            System.out.println("We'll see how well you did in the next stage");
         }
     }
 }
