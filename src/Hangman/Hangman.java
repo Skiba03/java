17ee7228eea2f7224dd6ab08d2186f554a437328
Zhenya <y.s.skiba@student.khai.edu>
2022-12-25 16:39:26 +0200

    Hangman 6

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,77 +1,54 @@
 package Hangman;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Hangman {
-
-    private static String[] words = {"java","python","javascript","kotlin",};
-    private static String word = words[(int) (Math.random() * words.length)];
-    private static String minus = new String(new char[word.length()]).replace("\0", "-");
-    private static int count = 0;
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
 
-        while (count < 8 && minus.contains("-")) {
-            System.out.println(minus);
-            String guess = scan.next();
-            hang(guess);
+        byte attemps = 8;
+
+        String[] dict = new String[]{"python", "java", "javascript", "kotlin"};
+        String hiddenWord = dict[(int) (Math.random() * dict.length)];
+        char[] hiddenWordArray = hiddenWord.toCharArray();
+        char[] hiddenWordChar = new char[hiddenWordArray.length];
+        for (int i = 0; i < hiddenWordArray.length; i++)
+            hiddenWordChar[i] = '-';
+        Scanner userAnswer = new Scanner(System.in);
+        while (attemps > 0) {
+            System.out.printf("You life: %d\nGuess the word %s: ", attemps, String.valueOf(hiddenWordChar));
+            char char_answer = userAnswer.nextLine().toCharArray()[0];
+                char[] hiddenWordChar_N = checkout(hiddenWordArray, hiddenWordChar.clone(), char_answer);
+            if (hiddenWordChar_N == null) {
+                attemps--;
+                System.out.println("That letter doesn`t appear in the word");
+            }
+            else if (Arrays.equals(hiddenWordChar_N, hiddenWordArray)) {
+                System.out.println("You win!");
+                break;
+        } else if (Arrays.equals(hiddenWordChar, hiddenWordChar_N)) {
+            System.out.println("No improvements");
+            attemps--;
+        } else if (!(boolean) Arrays.equals(hiddenWordChar_N, hiddenWordChar)) {
+            hiddenWordChar = hiddenWordChar_N.clone();
         }
-        scan.close();
     }
-
-    public static void hang(String guess) {
-        String newminus = "";
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == guess.charAt(0)) {
-                newminus += guess.charAt(0);
-            } else if (minus.charAt(i) != '-') {
-                newminus += word.charAt(i);
-            } else {
-                newminus += "-";
+        if (attemps <= 0)
+            System.out.println("You lost!");
+}
+    public static char[] checkout (char[] dict, char[] hiddenWordChar, char userAnswer) {
+        byte changed = 0;
+        for (int i = 0; i < hiddenWordChar.length; i++) {
+            if (userAnswer == dict[i]) {
+                hiddenWordChar[i] = userAnswer;
+                changed++;
             }
         }
-
-        if (minus.equals(newminus)) {
-            count++;
-            Try();
-        }else {
-            minus = newminus;
-            count++;
-        }
-        if (minus.equals(word)) {
-            System.out.println("You win!");
-            System.out.println("Thanks for playing!");
-            System.out.println("We'll see how well you did in the next stage");
-        }
-    }
-
-    public static void Try() {
-        if (count == 1) {
-            System.out.println("That letter doesn't appear in the word");
-        }
-        if (count == 2) {
-            System.out.println("That letter doesn't appear in the word");
-        }
-        if (count == 3) {
-            System.out.println("That letter doesn't appear in the word");
-        }
-        if (count == 4) {
-            System.out.println("That letter doesn't appear in the word");
-        }
-        if (count == 5) {
-            System.out.println("That letter doesn't appear in the word");
-        }
-        if (count == 6) {
-            System.out.println("That letter doesn't appear in the word");
-        }
-        if (count == 7) {
-            System.out.println("That letter doesn't appear in the word");
-        }
-        if (count == 8) {
-            System.out.println("Thanks for playing");
-            System.out.println("We'll see how well you did in the next stage");
-        }
+        if (changed != 0)
+            return hiddenWordChar;
+        else
+            return null;
     }
 }
