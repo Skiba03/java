c19f25acdeed62197b8875051ebf8740be49193a
Zhenya <y.s.skiba@student.khai.edu>
2023-01-01 16:06:42 +0200

    Hangman 7

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,12 +1,18 @@
 package Hangman;
 
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon.");
+
+        List<Character> used = new ArrayList<>();
 
         byte attemps = 8;
 
@@ -20,24 +26,26 @@ public class Hangman {
         while (attemps > 0) {
             System.out.printf("You life: %d\nGuess the word %s: ", attemps, String.valueOf(hiddenWordChar));
             char char_answer = userAnswer.nextLine().toCharArray()[0];
-                char[] hiddenWordChar_N = checkout(hiddenWordArray, hiddenWordChar.clone(), char_answer);
-            if (hiddenWordChar_N == null) {
-                attemps--;
-                System.out.println("That letter doesn`t appear in the word");
-            }
-            else if (Arrays.equals(hiddenWordChar_N, hiddenWordArray)) {
-                System.out.println("You win!");
-                break;
-        } else if (Arrays.equals(hiddenWordChar, hiddenWordChar_N)) {
-            System.out.println("No improvements");
-            attemps--;
-        } else if (!(boolean) Arrays.equals(hiddenWordChar_N, hiddenWordChar)) {
-            hiddenWordChar = hiddenWordChar_N.clone();
+            if (97 <= Character.hashCode(char_answer) & Character.hashCode(char_answer) <= 122) {
+                if (used.isEmpty() | !used.contains(char_answer)) {
+                    used.add(char_answer);
+                    char[] new_result_char = checkout(hiddenWordArray, hiddenWordChar.clone(), char_answer);
+                    if (new_result_char == null) {
+                        attemps--;
+                        System.out.println("That letter doesn't appear in the word");
+                    } else if (Arrays.equals(new_result_char, hiddenWordArray)) {
+                        System.out.println("You win!!!");
+                        break;
+                    } else if (!(boolean) Arrays.equals(new_result_char, hiddenWordChar))
+                        hiddenWordChar = new_result_char.clone();
+                } else if (used.contains(char_answer))
+                    System.out.println("You`ve already guessed this letter");
+            }else
+                System.out.println("Please enter a lowercase English letter");
         }
-    }
         if (attemps <= 0)
             System.out.println("You lost!");
-}
+    }
     public static char[] checkout (char[] dict, char[] hiddenWordChar, char userAnswer) {
         byte changed = 0;
         for (int i = 0; i < hiddenWordChar.length; i++) {
