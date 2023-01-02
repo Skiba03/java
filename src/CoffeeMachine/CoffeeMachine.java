61faef7d956cbeddda2c157ab8aed83503a0f3c1
Zhenya <y.s.skiba@student.khai.edu>
2023-01-02 15:30:35 +0200

    CoffeeMachine 3

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -3,13 +3,49 @@ package CoffeeMachine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
+    private int water;
+    private int milk;
+    private int beans;
+    private int cups;
+
     public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.Ingredients();
+        coffeeMachine.Cups();
+    }
+
+    public void Ingredients() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Write how many cups of coffee you will need:");
-        int input = scan.nextInt();
-        System.out.println("For"+ input +"cups of coffee you will need:");
-        System.out.println(input * 200 +"ml of water");
-        System.out.println(input * 50 +"ml of milk");
-        System.out.println(input * 15 +"g of coffee beans");
+        System.out.print("Write how many ml of water the coffee machine has:");
+        this.water = scan.nextInt();
+        System.out.print("Write how many ml of milk the coffee machine has:");
+        this.milk = scan.nextInt();
+        System.out.print("Write how many grams of coffee beans the coffee machine has:");
+        this.beans = scan.nextInt();
+        System.out.print("Write how many cups of coffee you will need:");
+        this.cups = scan.nextInt();
+    }
+
+    public void Cups(){
+        int coffeeCups = 0;
+
+        while (true) {
+            if (water >= 200 && milk >= 50 && beans >= 15) {
+                water -= 200;
+                milk -= 50;
+                beans -= 15;
+            } else {
+                break;
+            }
+            coffeeCups++;
+        }
+
+        if (coffeeCups < cups) {
+            System.out.println("No, I can make only " + cups + " cups of coffee");
+        } else if (coffeeCups == cups) {
+            System.out.println("Yes, I can make that amount of coffee.");
+        } else {
+            System.out.println("Yes, I can make that amount of coffee (and even " + (coffeeCups - cups) + " more than that)");
+        }
     }
 }
